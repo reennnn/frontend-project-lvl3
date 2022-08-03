58944227afa17011896175ac3c8179cbb2bffc4f
rinat <r.bashirov@zenlabs.biz>
2022-08-03 21:19:16 +0500

    linter fixes

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
 import * as yup from 'yup';
-import watched from './view.js';
+import watched from './view';
 
 export default () => {
-    const state = {
-        links: [],
-        form: {
-            validateState: 'valid', //could be valid, invalid, duplicate
-            process: 'filling', //could be filling, failed, processed, successful
-            processError: null,
-            error: '',
-            url: '',
-        },
-    };
-    const watchedState = watched(state);
-    const schema = yup.object().shape({
-        website: yup.string().url(),
-    });
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
+  const state = {
+    links: [],
+    form: {
+      validateState: 'valid', // could be valid, invalid, duplicate
+      process: 'filling', // could be filling, failed, processed, successful
+      processError: null,
+      error: '',
+      url: '',
+    },
+  };
+  const watchedState = watched(state);
+  const schema = yup.object().shape({
+    website: yup.string().url(),
+  });
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const value = formData.get('url');
     schema.validate({ website: value }).then(() => {
-        if (!watcherState.links.includes(value)) {
-            watchedState.links.unshift(value);
-            watchedState.from.valdationState = 'valid';
-        } else {
-            watchedState.form.valdationState = 'duplicate';
-            watchedState.form.error = 'RSS уже существует';
-            watchedState.form.process = 'failed';
-        }
-    }).catch(() => {
-        watchedState.form.valdationState = 'invalid';
-        watchedState.form.error = 'Ссылка должна быть валидным URL';
+      if (!watchedState.links.includes(value)) {
+        watchedState.links.unshift(value);
+        watchedState.from.valdationState = 'valid';
+      } else {
+        watchedState.form.valdationState = 'duplicate';
+        watchedState.form.error = 'RSS уже существует';
         watchedState.form.process = 'failed';
+      }
+    }).catch(() => {
+      watchedState.form.valdationState = 'invalid';
+      watchedState.form.error = 'Ссылка должна быть валидным URL';
+      watchedState.form.process = 'failed';
     });
     form.reset();
     document.getElementById('url-input').focus();
--- src/view.js
+++ src/view.js
@@ -1,31 +1,31 @@
-import onChange from "on-change";
+import onChange from 'on-change';
 
 const watchedState = (state) => onChange(state, (path, value) => {
-    const feedback = document.querySelector('p.feedback');
-    const input = document.getElementById('url-input');
-    //const form = document.querySelector('form');
-    //const button = document.querySelector('[aria-label="add"]');
+  const feedback = document.querySelector('p.feedback');
+  const input = document.getElementById('url-input');
+  // const form = document.querySelector('form');
+  // const button = document.querySelector('[aria-label="add"]');
 
-    if (path === 'form.validationState') {
-        if (value === 'invalid') {
-            input.classList.add('is-invalid');
-            feedback.classList.add('text-danger');
-            feedback.classList.remove('text-scuccess');
-            feedback.textContent = 'Ссылка должна быть валидным URL';
-        }
-        if (value === 'duplicate') {
-            input.classList.add('is-invalid');
-            feedback.classList.add('text-danger');
-            feedback.classList.remove('text-success');
-            feedback.textContent = 'RSS уже существует'
-        }
+  if (path === 'form.validationState') {
+    if (value === 'invalid') {
+      input.classList.add('is-invalid');
+      feedback.classList.add('text-danger');
+      feedback.classList.remove('text-scuccess');
+      feedback.textContent = 'Ссылка должна быть валидным URL';
     }
-   // if (path === 'form.process') {
-   //     if (value === 'failed') {
-   //         input.setAttribute('readonly', true);
-   //         button.setAttribute('disabled', true)
-   //     }
-   // }
+    if (value === 'duplicate') {
+      input.classList.add('is-invalid');
+      feedback.classList.add('text-danger');
+      feedback.classList.remove('text-success');
+      feedback.textContent = 'RSS уже существует';
+    }
+  }
+  // if (path === 'form.process') {
+  //     if (value === 'failed') {
+  //         input.setAttribute('readonly', true);
+  //         button.setAttribute('disabled', true)
+  //     }
+  // }
 });
 
 export default watchedState;
